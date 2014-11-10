cf6c6f558bac9943279dfb319be72a989ab3d869
ms elouafi <malfunction.pl-mn@outlook.com>
2014-11-10 20:42:47 +0100

    add second third p costumized alittle bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Pinterest::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
+  get "bored" => "pages#bored"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
